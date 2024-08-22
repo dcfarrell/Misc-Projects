b9823ddba96a2914c0c60f325ffc7199d2fb7d2b
dcfarrell <delaneycfarrell@gmail.com>
2024-08-22 17:22:54 -0500

    Various Projects

---
+++ color2.cpp
@@ -0,0 +1,8 @@
+//
+//  color2.cpp
+//  
+//
+//  Created by Delaney Farrell on 4/1/19.
+//
+
+#include "color2.hpp"
---
+++ color2.hpp
@@ -0,0 +1,13 @@
+//
+//  color2.hpp
+//  
+//
+//  Created by Delaney Farrell on 4/1/19.
+//
+
+#ifndef color2_hpp
+#define color2_hpp
+
+#include <stdio.h>
+
+#endif /* color2_hpp */
